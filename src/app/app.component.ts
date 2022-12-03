9d3f56ec6ade59a60421126d6423356fc6354468
ichathux <ichathux@gmail.com>
2022-12-04 01:52:07 +0530

    data retrieval done & created modal and call madal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { error } from 'console';
+import { response } from 'express';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employeemanagerapp';
+export class AppComponent implements OnInit{
+  public employees: Employee[] = [];
+  constructor(private employeeService: EmployeeService){}
+
+  ngOnInit(){
+    this.getEmployees();
+  }
+
+  public getEmployees():void{
+    this.employeeService.getEmployees().subscribe(
+      (response : Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+  public onOpenCreateEmpNModal():void{
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    button.setAttribute('data-target', '#addEmployeeModal'); 
+    container?.appendChild(button);
+    button.click();
+  }
+
+  public onOpenModel(employee : Employee, mode:string):void{
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if(mode === 'add'){
+      button.setAttribute('data-target', '#addEmployeeModal'); 
+    }
+    if(mode === 'edit'){
+      button.setAttribute('data-target', '#editEmployeeModal'); 
+    }
+    if(mode === 'delete'){
+      button.setAttribute('data-target', '#deleteEmployeeModal'); 
+    }
+    container?.appendChild(button);
+    button.click();
+  }
+
 }
